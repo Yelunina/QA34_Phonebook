849bfd5f29f36d6dc7694f68c2bf18ee7acb6bf9
Iryna Yelunina <lucky.asterisk@gmail.com>
2024-04-07 20:07:18 +0200

    update

--- src/test/java/com/phonebook/tests/HomePageTests.java
+++ src/test/java/com/phonebook/tests/HomePageTests.java
@@ -3,6 +3,7 @@ package com.phonebook.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -27,8 +28,20 @@ public class HomePageTests {
     }
 
     @Test
-    public void isHomeComponentPresent() {
-        driver.findElement(By.cssSelector("div:nth-child(2) div h1"));
+    public void isHomeComponentPresentTest() {
+        // driver.findElement(By.cssSelector("div:nth-child(2) div h1"));
+        // System.out.println("Home component is " + isHomeComponentPresent());
+        Assert.assertTrue(isHomeComponentPresent());
+
     }
+
+       public boolean isHomeComponentPresent() {
+        return driver.findElements(By.cssSelector("div:nth-child(2) div h1")).size() > 0;
+    }
+
+    public boolean isElementPresent(By locator) {
+        return driver.findElements(locator).size() > 0;
+    }
+
 
 }
